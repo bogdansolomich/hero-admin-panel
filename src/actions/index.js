54236293f66824553f804df3e654dce7ba12682d
bogdansolomich <bogdansolomich@gmail.com>
2025-01-10 16:41:35 +0200

    Через createSlice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
-import { createAction } from '@reduxjs/toolkit';
-
+// import { createAction } from '@reduxjs/toolkit';
+import {
+  heroesFetching,
+  heroesFetched,
+  heroesFetchingError,
+} from '../components/heroesList/heroesSlice';
+import {
+  filtersFetching,
+  filtersFetched,
+  filtersFetchingError,
+} from '../components/heroesFilters/filtersSlice';
 export const fetchHeroes = request => dispatch => {
   dispatch(heroesFetching());
   request('http://localhost:3001/heroes')
@@ -20,57 +29,86 @@ export const fetchFilters = request => dispatch => {
 //   };
 // };
 
-const heroesFetching = createAction('HEROES_FETCHING');
+// export const heroesFetching = createAction('HEROES_FETCHED');
+// export const heroesFetching = createAction('HEROES_FETCHING');
+// export const heroesFetched = createAction('HEROES_FETCHED');
+// export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
+// export const heroCreated = createAction('HERO_CREATED');
+// export const heroDeleted = createAction('HERO_DELETED');
+// export const filtersFetching = () => {
+//   return {
+//     type: 'FILTERS_FETCHING',
+//   };
+// };
 
-export const heroesFetched = heroes => {
-  return {
-    type: 'HEROES_FETCHED',
-    payload: heroes,
-  };
-};
+// export const filtersFetched = filters => {
+//   return {
+//     type: 'FILTERS_FETCHED',
+//     payload: filters,
+//   };
+// };
 
-export const heroesFetchingError = () => {
-  return {
-    type: 'HEROES_FETCHING_ERROR',
-  };
-};
+// export const filtersFetchingError = () => {
+//   return {
+//     type: 'FILTERS_FETCHING_ERROR',
+//   };
+// };
 
-export const filtersFetching = () => {
-  return {
-    type: 'FILTERS_FETCHING',
-  };
-};
+// export const activeFilterChanged = filter => {
+//   return {
+//     type: 'ACTIVE_FILTER_CHANGED',
+//     payload: filter,
+//   };
+// };
+// export const heroesFetched = heroes => {
+//   return {
+//     type: 'HEROES_FETCHED',
+//     payload: heroes,
+//   };
+// };
 
-export const filtersFetched = filters => {
-  return {
-    type: 'FILTERS_FETCHED',
-    payload: filters,
-  };
-};
+// export const heroesFetchingError = () => {
+//   return {
+//     type: 'HEROES_FETCHING_ERROR',
+//   };
+// };
 
-export const filtersFetchingError = () => {
-  return {
-    type: 'FILTERS_FETCHING_ERROR',
-  };
-};
+// export const filtersFetching = () => {
+//   return {
+//     type: 'FILTERS_FETCHING',
+//   };
+// };
 
-export const activeFilterChanged = filter => {
-  return {
-    type: 'ACTIVE_FILTER_CHANGED',
-    payload: filter,
-  };
-};
+// export const filtersFetched = filters => {
+//   return {
+//     type: 'FILTERS_FETCHED',
+//     payload: filters,
+//   };
+// };
 
-export const heroCreated = hero => {
-  return {
-    type: 'HERO_CREATED',
-    payload: hero,
-  };
-};
+// export const filtersFetchingError = () => {
+//   return {
+//     type: 'FILTERS_FETCHING_ERROR',
+//   };
+// };
 
-export const heroDeleted = id => {
-  return {
-    type: 'HERO_DELETED',
-    payload: id,
-  };
-};
+// export const activeFilterChanged = filter => {
+//   return {
+//     type: 'ACTIVE_FILTER_CHANGED',
+//     payload: filter,
+//   };
+// };
+
+// export const heroCreated = hero => {
+//   return {
+//     type: 'HERO_CREATED',
+//     payload: hero,
+//   };
+// };
+
+// export const heroDeleted = id => {
+//   return {
+//     type: 'HERO_DELETED',
+//     payload: id,
+//   };
+// };
--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -4,16 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { createSelector } from 'reselect';
 
-import { fetchHeroes, heroDeleted } from '../../actions';
+import { fetchHeroes } from '../../actions';
+import { heroDeleted } from './heroesSlice';
+
 import HeroesListItem from '../heroesListItem/HeroesListItem';
 import Spinner from '../spinner/Spinner';
 
 import './heroesList.scss';
-
-// Задача для этого компонента:
-// При клике на "крестик" идет удаление персонажа из общего состояния
-// Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
   const filteredHeroesSelector = createSelector(
@@ -21,7 +18,6 @@ const HeroesList = () => {
     state => state.heroes.heroes,
     (filter, heroes) => {
       if (filter === 'all') {
-        // console.log('render');
         return heroes;
       } else {
         return heroes.filter(item => item.element === filter);
@@ -29,16 +25,6 @@ const HeroesList = () => {
     }
   );
 
-  // const filteredHeroes = useSelector(state => {
-  //   if (state.filters.activeFilter === 'all') {
-  //     console.log('render');
-  //     return state.heroes.heroes;
-  //   } else {
-  //     return state.heroes.heroes.filter(
-  //       item => item.element === state.filters.activeFilter
-  //     );
-  //   }
-  // });
   const filteredHeroes = useSelector(filteredHeroesSelector);
   const heroesLoadingStatus = useSelector(
     state => state.heroes.heroesLoadingStatus
@@ -48,23 +34,17 @@ const HeroesList = () => {
 
   useEffect(() => {
     dispatch(fetchHeroes(request));
-
     // eslint-disable-next-line
   }, []);
 
-  // Функция берет id и по нему удаляет ненужного персонажа из store
-  // ТОЛЬКО если запрос на удаление прошел успешно
-  // Отслеживайте цепочку действий actions => reducers
   const onDelete = useCallback(
     id => {
-      // Удаление персонажа по его id
       request(`http://localhost:3001/heroes/${id}`, 'DELETE')
         .then(data => console.log(data, 'Deleted'))
         .then(dispatch(heroDeleted(id)))
         .catch(err => console.log(err));
-      // eslint-disable-next-line
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line
     [request]
   );
 
